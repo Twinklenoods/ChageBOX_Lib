03a3fe6a639950fae590d44885d0335d010d13f0
Twinkle <seed6408@gmail.com>
2020-07-02 08:57:36 +0800

    Revert "繼承&封裝&over riding"
    This reverts commit 87c7e9eba7d6280b474d761adf7d4061ae99d056.

--- ChageBOX_Lib/src/uuu/vgb/test/TestCustomer.java
+++ ChageBOX_Lib/src/uuu/vgb/test/TestCustomer.java
@@ -2,31 +2,25 @@ package uuu.vgb.test;
 
 
 
-import java.time.LocalDate;
-
 import uuu.vgb.entity.Customer;
 
 public class TestCustomer {
 
 	public static void main(String[] args) {
-		Customer cus = new Customer("A1023456789","123456",LocalDate.of(1990,4,1));
-		
-		
-		//Customer cus = new Customer();
-		//cus.setId("A123456789");
+		Customer cus = new Customer();
+		cus.setId("A123456789");
 		cus.setName("吳大頭");
-		//cus.setPassword("123456");
+		cus.setPassword("123456");
 		cus.setGender('M');
 		cus.setEmail("astrayx56408@gmail.com");
-		//cus.setBirthday=LocalDate.of(1990,4,1);
-		//cus.setBirthday(LocalDate.parse("19900404"));
-		//String bDate= "2999-06-09";
-		//cus.setBirthday("2999-06-09");
+		//cus.setbirthday=LocalDate.of(1990,4,1);
+		String bDate= "1999-06-09";
+		cus.setBirthday(bDate);
 		System.out.println(cus.getBirthday());
 		
 		//cus.birthday=LocalDate.now();
 		//cus.birthday=LocalDate.parse("1990-04-01");
-		cus.setPhone("0228910991");
+		cus.setPhone("0912294389");
 		System.out.println(cus.getPhone());
 		System.out.println(cus.getId());
 		System.out.println(cus.getName());
@@ -35,11 +29,11 @@ public class TestCustomer {
 		System.out.println(cus.getEmail());
 		System.out.println(cus.getBirthday()!=null?
 				cus.getBirthday().getDayOfWeek():"");
-		//boolean a =(cus.cheakID(cus.getId()));
+		boolean a =(cus.cheakID(cus.getId()));
 		
-		//System.out.println(a?cus.getId()+"正確":cus.getId()+"不正確");//boolean?true:false
-		//System.out.println(a); 
-		
+		System.out.println(a?cus.getId()+"正確":cus.getId()+"不正確");//boolean?true:false
+		System.out.println(a); 
+		System.out.println("id"+cus.cheakID(cus.getId()));
 		System.out.println(cus.getBirthday());
 		System.out.println(cus.getAddress());
 	}
