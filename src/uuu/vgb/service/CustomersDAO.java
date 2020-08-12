5067164595c903d03ab66f63fa84362195674732
TWINKLE\淫魔 <b09828035@mail.knu.edu.tw>
2020-08-12 14:57:17 +0800

    整理資料順序

---
+++ src/uuu/vgb/service/CustomersDAO.java
@@ -0,0 +1,92 @@
+package uuu.vgb.service;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+
+import uuu.vgb.entity.Customer;
+import uuu.vgb.entity.VGBException;
+class CustomersDAO {
+	
+	
+	private static final String INSERT_CUSTOMER= "   insert INTO customers" + 
+			"(id, name,password,gender,email,birthday,"
+			+ "address,phone)"
+			+ 
+			"    VALUES(?,?,?,?,?,?,?,?)";
+	public void insert(Customer c) throws VGBException{
+		
+		try (
+			Connection connection =RDBConnection.getConnection();//1.2.取得連線
+			PreparedStatement pstmt =connection.prepareStatement(INSERT_CUSTOMER);//3.準備指令
+		){
+			//3.1傳入?值
+			pstmt.setString(1,c.getId());//3.1
+			pstmt.setString(2,c.getName());
+			pstmt.setString(3,c.getPassword());
+			pstmt.setString(4,String.valueOf(c.getGender()));			
+			pstmt.setString(5,c.getEmail());
+			pstmt.setString(6,c.getBirthday().toString());			
+			pstmt.setString(7,c.getPhone());
+			pstmt.setString(8,c.getAddress());
+			//pstmt.setBoolean(9,c.isMarried());
+			
+			 pstmt.executeUpdate();//4.執行指令
+			
+		} catch (SQLIntegrityConstraintViolationException e) {
+			String key="";
+			if(e.getMessage().indexOf("email_UNIQUE")>=0) {
+				key="email";
+			}else if(e.getMessage().indexOf("PRIMSRY")>=0) {
+				key="id";
+			}
+			throw new VGBException("帳號已重複-"+key+"重複註冊",e);
+		} catch (SQLException e) {
+				throw new VGBException("客戶新增失敗",e);
+		} 
+			
+		
+	}
+
+
+	private static final String SELECT_CUSTOMER_BY_ID="SELECT  id, name, password, gender, email, address, birthday,phone,"
+			+ "married,discount, class_name FROM vgb.customers"
+			+" WHERE id=?";
+	 Customer selectCustomersById(String id) throws VGBException {
+		Customer c = null;
+		
+		
+		try (
+			Connection connection =RDBConnection.getConnection();//1.2取得連線物件
+			PreparedStatement pstmt =connection.prepareStatement(SELECT_CUSTOMER_BY_ID);//3.準備指令
+		){
+			//3.1
+			pstmt.setString(1, id);
+			
+			//4.執行指令
+			try(
+					ResultSet rs= pstmt.executeQuery();
+					){
+						//5.處理rs
+				while(rs.next()) {
+						c=new Customer();
+						
+						c.setId(rs.getString("id"));
+						c.setName(rs.getString("name"));
+						c.setPassword(rs.getString("password"));
+						c.setGender(rs.getString("gender").charAt(0));
+						c.setEmail(rs.getString("email"));
+						c.setBirthday(rs.getString("birthday"));
+						
+				}
+					}
+				} catch (SQLException e) {
+					throw new VGBException("用主鑑值查詢客戶失敗",e);		
+				}
+				 
+				return c;
+			}
+
+		}
---
+++ src/uuu/vgb/service/RDBConnection.java
@@ -0,0 +1,52 @@
+package uuu.vgb.service;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import uuu.vgb.entity.VGBException;
+
+
+class RDBConnection {
+	private final static String driver ="com.mysql.cj.jdbc.Driver";
+	private final static String url="jdbc:mysql://localhost:3306/vgb?serverTimezone=UTC";
+	private final static String user="root";
+	private final static String password="6408";
+	
+	public static Connection getConnection() throws VGBException{
+		try {
+			Class.forName(driver);//1.載入Driver
+			
+			try {
+				Connection  connection =DriverManager.getConnection(url,user,password);//2.建立連線
+				return connection;
+			} catch (SQLException e) {
+				//System.out.println("建立連線失敗");
+				throw new VGBException("建立連線失敗",e);
+				
+			}
+		} catch (ClassNotFoundException e) {
+			//System.out.println("載入Drive失敗");
+			throw new VGBException("載入Drive失敗",e);
+			
+			
+			
+			//e.printStackTrace();
+		}
+		//return null;//是不恰當的寫法
+	}
+	public static void main(String[] args) {
+		
+		try(
+				Connection con = RDBConnection.getConnection();
+		){
+			
+				System.out.println(con.getCatalog());
+			} catch (SQLException e) {				
+				e.printStackTrace();
+			}catch(VGBException e){
+				System.out.println("非預期錯誤"+e);
+			}
+		}
+	} 
+
+
