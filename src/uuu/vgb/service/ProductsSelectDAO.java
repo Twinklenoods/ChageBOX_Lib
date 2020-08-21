bb8dc3c8fef9ab6d7606ce7a5077d856ab7b3bb0
Joe Wu <99390124@ss25.mcu.edu.tw>
2020-08-21 22:58:09 +0800

    新增LOG用法

--- src/uuu/vgb/service/ProductsSelectDAO.java
+++ src/uuu/vgb/service/ProductsSelectDAO.java
@@ -2,12 +2,14 @@ package uuu.vgb.service;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 import uuu.vgb.entity.Product;
 import uuu.vgb.entity.VGBException;
 
 class ProductsSelectDAO {
 
+	private Logger LOG = Logger.getLogger(ProductsSelectDAO.class.getName());
 	private static final String SELECT_ALL_PRODUCTS="SELECT"
 			+ " id,name,description,origin,ownerN,owner,host,unitprice,createTime,buy,wantChange,photoUrl,customer" 
 			+ "	FROM products";
@@ -15,7 +17,8 @@ class ProductsSelectDAO {
 	List<Product> selectAllProducts() throws VGBException{
 		List<Product> list =new ArrayList<>();
 		
-		
+
+		LOG.info("selectAllProducts");
 		try (Connection conn =RDBConnection.getConnection();
 				Statement stmt =conn.createStatement();//.3建立指令
 				ResultSet rs= stmt.executeQuery(SELECT_ALL_PRODUCTS);
@@ -46,6 +49,8 @@ class ProductsSelectDAO {
 				p.setOwnerN(rs.getString("ownerN"));
 				
 				list.add(p);
+
+				LOG.info(p.toString());
 			}
 	
 		
@@ -63,7 +68,8 @@ class ProductsSelectDAO {
 			
 	Product selectProductsById(String id) throws VGBException {
 		Product c = null;
-		
+
+		LOG.info("selectProductsById");
 		
 		try (
 			Connection connection =RDBConnection.getConnection();//1.2取得連線物件
@@ -93,11 +99,13 @@ class ProductsSelectDAO {
 						c.setChangebox(rs.getString("Changebox"));
 						c.setOwnerN(rs.getString("ownerN"));
 						
-						
+						LOG.info("有東西！");
 						
 				}
 					}
 				} catch (SQLException e) {
+
+					LOG.info(e.toString());
 					throw new VGBException("用產品編號查詢失敗",e);		
 				}
 				 
@@ -218,6 +226,8 @@ class ProductsSelectDAO {
 			
 	public  List<Product> selectProductsUpdown(String updown) throws VGBException{
 		List<Product> list =new ArrayList<>();
+
+		LOG.info("selectProductsUpdown");
 		try(
 				Connection connection =RDBConnection.getConnection();//1.2
 				PreparedStatement pstmt =connection.prepareStatement(SELECT_PRODUYS_BY_UPDOWN);//3
@@ -247,8 +257,7 @@ class ProductsSelectDAO {
 					p.setUpdown(rs.getString("updown"));
 					list.add(p);
 					
-					
-					
+				
 					
 					
 				}
